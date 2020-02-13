d883bad4a918a24d7dd1ac7758a1b4a164bc5636
unknown <60944051+tdicks1@users.noreply.github.com>
2020-02-13 15:55:45 -0600

    AssignmentDicks

---
+++ ClassAssignment2-13.ps1
@@ -0,0 +1,33 @@
+﻿# Install NuGet provider
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+# Trust the PSGallery source
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+# Decide to install or update on module already present
+# If loop checks if the module already exists
+if ((Get-Module -Name 'SpeculationControl')){
+    Write-Host "Module is already imported. Updating Existing Module."# returns True
+    Update-Module SpeculationControl
+}
+#If the module does not exist, the module is installed.
+else {
+    Write-Host "Module is does not exist. Importing module now." # returns False
+    Install-Module SpeculationControl
+}
+    
+
+#Check for vulnerabilities
+Get-SpeculationControlSettings
+
+# TODO: Error handling (i.e. no Internet connection)
+#if loop tests internet connection by attempting to connect to google.com
+if (Test-NetConnection -ComputerName www.google.com) {
+    Write-Host "Internet connection successful." #if the connection succeeds
+}
+
+else {
+    Write-Host "Internet connection failed." #if the connection fails
+}
+
+#git commit and git push to push script to github
